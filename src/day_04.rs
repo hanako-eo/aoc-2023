1436f99853cfccb65363e1aabfc519e393b2348d
hanako-eo <enuxura@gmail.com>
2023-12-05 14:23:35 +0100

    Jour 4 partie 2

--- src/day_04.rs
+++ src/day_04.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -24,7 +25,7 @@ impl Runner for AocDay04 {
             .filter_map(|numbers| numbers.split_once(" | ").map(|(a, b)| (a.to_owned(), b.to_owned())))
             .map(|(numbers, winning_numbers)| (
                 numbers.split_ascii_whitespace().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<i32>>(),
-                winning_numbers.split_ascii_whitespace().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<i32>>(),
+                winning_numbers.split_ascii_whitespace().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<i32>>()
             ))
             .collect::<Vec<(Vec<i32>, Vec<i32>)>>();
     }
@@ -48,7 +49,26 @@ impl Runner for AocDay04 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        let mut scalars = HashMap::<usize, u32>::new();
+        for (i, (numbers, winning_numbers)) in self.cards.iter().enumerate() {
+            // Nombre de nombre gagnant
+            let mut winning_numbers_count = 0;
+            for number in numbers {
+                if winning_numbers.contains(number) {
+                    winning_numbers_count += 1;
+                }
+            }
+
+            let scalar = *scalars.entry(i).or_insert(1);
+
+            for j in 0..winning_numbers_count.min(self.cards.len()) {
+                scalars.entry(j + i + 1)
+                    .and_modify(|next_card_scalar| *next_card_scalar += scalar)
+                    .or_insert(scalar + 1);
+            }
+        }
+
+        aoclib::output(scalars.values().sum::<u32>())
     }
 }
 
@@ -65,12 +85,11 @@ fn day_04_part1() {
 
 #[test]
 fn day_04_part2() {
-    // let mut day = AocDay04::new();
+    let mut day = AocDay04::new();
     
-    // let file = File::open("./input/day-04-test.txt").expect("Pour avancer, il faut le fichier day-04.txt");
-    // let buffer = BufReader::new(file);
-    
-    // day.parse_schema(buffer);
-    
-    // assert_eq!(day.part2(), vec!["467835".to_string()]);
+    let file = File::open("./input/day-04-test.txt").expect("Pour avancer, il faut le fichier day-04.txt");
+
+    day.parse(file);
+
+    assert_eq!(day.part2(), vec!["30".to_string()]);
 }
