bb2030e5191393892ddd00a4ef9e97876415ea85
hanako-eo <enuxura@gmail.com>
2023-12-08 16:33:56 +0100

    Jour 6 partie 2

--- src/day_06.rs
+++ src/day_06.rs
@@ -2,17 +2,13 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::ops::Range;
-use std::sync::atomic::{AtomicU64, Ordering};
-
-use range_union_find::RangeUnionFind;
-use rayon::prelude::*;
 
 use aoclib::Runner;
 
 #[derive(Default)]
 pub struct AocDay06 {
-    times: Vec<u32>,
-    ditances: Vec<u32>,
+    times: String,
+    distances: String,
 }
 
 impl AocDay06 {
@@ -32,36 +28,43 @@ impl Runner for AocDay06 {
             .unwrap()
             .split_once(":")
             .unwrap().1
-            .split_ascii_whitespace()
-            .map(|n| n.parse::<u32>().unwrap())
-            .collect::<Vec<u32>>();
+            .to_string();
 
-        self.ditances = lines.next()
+        self.distances = lines.next()
             .unwrap()
             .split_once(":")
             .unwrap().1
-            .split_ascii_whitespace()
-            .map(|n| n.parse::<u32>().unwrap())
-            .collect::<Vec<u32>>();
+            .to_string();
     }
 
     fn part1(&mut self) -> Vec<String> {
         let mut power_of_number_records = 1;
-        for (i, time) in self.times.iter().enumerate() {
+        let distances = self.distances.split_ascii_whitespace().map(|n| n.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+        for (i, time) in self.times.split_ascii_whitespace().map(|n| n.parse::<u32>().unwrap()).enumerate() {
             let mut number_record = 0;
-            for t in 0..=*time {
-                if self.ditances[i] < t * (time - t) {
+            for t in 0..=time {
+                if distances[i] < t * (time - t) {
                     number_record += 1;
                 }
             }
             power_of_number_records *= number_record;
         }
-
+        
         aoclib::output(power_of_number_records)
     }
     
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        let time = self.times.split_ascii_whitespace().collect::<Vec<&str>>().join("").parse::<u64>().unwrap();
+        let distance = self.distances.split_ascii_whitespace().collect::<Vec<&str>>().join("").parse::<u64>().unwrap();
+        
+        let mut number_record = 0;
+        for t in 0..=time {
+            if distance < t * (time - t) {
+                number_record += 1;
+            }
+        }
+
+        aoclib::output(number_record)
     }
 }
 
@@ -76,13 +79,13 @@ fn day_06_part1() {
     assert_eq!(day.part1(), vec!["288".to_string()]);
 }
 
-// #[test]
-// fn day_06_part2() {
-//     let mut day = AocDay06::new();
+#[test]
+fn day_06_part2() {
+    let mut day = AocDay06::new();
     
-//     let file = File::open("./input/day-06-test.txt").expect("Pour avancer, il faut le fichier day-06.txt");
+    let file = File::open("./input/day-06-test.txt").expect("Pour avancer, il faut le fichier day-06.txt");
 
-//     day.parse(file);
+    day.parse(file);
 
-//     assert_eq!(day.part2(), vec!["46".to_string()]);
-// }
+    assert_eq!(day.part2(), vec!["71503".to_string()]);
+}
